13d19a111e225c119d6f8659b19fe46f27ef977e
Valera Boldarev <vboldarev@weltkind.com>
2023-04-28 12:21:11 +0600

    happycilm

--- js/core.js
+++ js/core.js
@@ -1,30 +1,4 @@
 $(document).ready(function () {
-	// слaйдер на главной
-	$('.slider').slick({
-		slidesToScroll:1,
-		slidesToShow:1,
-		// autoplay: true,
-		// appendArrows:'.slider-arrows',
-		autoplaySpeed: 3000,
-		pauseOnHover:true,
-		arrows:true,
-		dots:false,
-		responsive: [
-			{
-				breakpoint: 767,
-				settings: {
-					arrows:true,
-				}
-			}
-		]
-	});
-
-	/*кнопка вверх*/
-	$('.scrollup').click(function(){
-		$("html, body").animate({ scrollTop: 0 }, 600);
-		return false;
-	});
-
 	/*мобильное меню*/
 	var mobileSidebar = function()
 	{
@@ -61,34 +35,4 @@ $(document).ready(function () {
 		return new obj();
 	}();
 	mobileSidebar.init();
-
-	/*аккордеон*/
-	$(".set > .set__btn").on("click", function(){
-		if($(this).hasClass('active')) {
-			$(this).removeClass("active");
-			$(this).siblings('.set__content').slideUp(200);
-		}
-		else{
-			$(".set > .set__btn").removeClass("active");
-			$(this).addClass("active");
-			$('.set__content').slideUp(200);
-			$(this).siblings('.set__content').slideDown(200);
-		}
-		return false
-	});
-	/*аккордеон внутри аккордеона*/
-	$(".inner__set > .inner__set_btn").on("click", function(){
-		if($(this).hasClass('active')) {
-			$(this).removeClass("active");
-			$(this).siblings('.inner__set_content').slideUp(200);
-		}
-		else{
-			$(".inner__set > .inner__set_btn").removeClass("active");
-			$(this).addClass("active");
-			$('.inner__set_content').slideUp(200);
-			$(this).siblings('.inner__set_content').slideDown(200);
-		}
-		return false
-	});
-
 });
