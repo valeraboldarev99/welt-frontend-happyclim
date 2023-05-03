7174be42411531a9fc892bb55ad4c32083bf979f
Valera Boldarev <vboldarev@weltkind.com>
2023-05-03 10:46:46 +0600

    cookie asker

--- js/core.js
+++ js/core.js
@@ -35,4 +35,35 @@ $(document).ready(function () {
 		return new obj();
 	}();
 	mobileSidebar.init();
+
+	// cookie asker
+	function setCookie(name, value, days) {
+		let expires = "";
+		if (days) {
+			let date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toUTCString();
+		}
+		document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	}
+	function getCookie(name) {
+		let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+	function checkCookies() {
+		let cookieNote = document.getElementById('cookie__note'); //окно cookie
+		let cookieBtnAccept = cookieNote.querySelector('.cookie__accept'); //кнопка согласия
+		// Если куки cookies_policy нет или она просрочена, то показываем уведомление
+		if (!getCookie('cookies_policy')) {
+			cookieNote.classList.add('show'); //класс для показа/скрытия окна куки
+		}
+		// При клике на кнопку устанавливаем куку cookies_policy на один год
+		cookieBtnAccept.addEventListener('click', function () {
+			setCookie('cookies_policy', 'true', 365);
+			cookieNote.classList.remove('show'); //класс для показа/скрытия окна куки
+		});
+	}
+	checkCookies();
+	// end cookie asker
+
 });
